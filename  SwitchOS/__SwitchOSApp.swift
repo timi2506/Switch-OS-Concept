c37589209afb684449dae4aee0b58caa7b6a5def
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-05-01 08:14:58 +0200

    Initial Commit

---
+++  SwitchOS/__SwitchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  __SwitchOSApp.swift
+//   SwitchOS
+//
+//  Created by Tim Schuchardt on 01.05.25.
+//
+
+import SwiftUI
+
+@main
+struct __SwitchOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
